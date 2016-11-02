a66670e63a23f8be8b889b014d100454e580f101
bohdanD <doroshbohdan1@gmail.com>
2016-11-02 15:57:07 +0200

    add behind generating database(if not exist)

--- projUI/MainWindow.xaml.cs
+++ projUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using projUI.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +25,89 @@ namespace projUI
         public MainWindow()
         {
             InitializeComponent();
+
+            if (CheckDataBaseExist())
+            {
+                MessageBox.Show("");
+            }
+            else
+                GenerateDataBase();
+
             DataContext = new MainWindowViewModel();
         }
 
-       
+
+
+        private bool CheckDataBaseExist()
+        {
+            SqlConnection Connection = new SqlConnection(@"data source = (LocalDb)\MSSQLLocalDB; initial catalog = projUI.DataModels.DataContext; integrated security = True; MultipleActiveResultSets = True; App = EntityFramework");
+            try
+            {
+                Connection.Open();
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+
+
+
+
+        private void GenerateDataBase()
+        {
+            List<string> cmds = new List<string>();
+            string script = System.AppDomain.CurrentDomain.BaseDirectory + "\\script.sql";
+            if (File.Exists(script))
+            {
+                TextReader tr = new StreamReader(script);
+                string line = "";
+                string cmd = "";
+                while ((line = tr.ReadLine()) != null)
+                {
+                    if (line.Trim().ToUpper() == "GO")
+                    {
+                        cmds.Add(cmd);
+                        cmd = "";
+                    }
+                    else
+                    {
+                        cmd += line = "\r\n";
+                    }
+                }
+                if (cmd.Length>0)
+                {
+                    cmds.Add(cmd);
+                    cmd = "";
+                }
+                tr.Close();
+            }
+            if (cmds.Count > 0)
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+                command.CommandType = System.Data.CommandType.Text;
+                command.Connection.Open();
+                for (int i = 0; i < cmds.Count; i++)
+                {
+                    command.CommandText = cmds[i];
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
